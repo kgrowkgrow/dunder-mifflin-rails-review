3373132013e8f18acff61fd2e641f3d9bee746e3
kgrowkgrow <kevingrow11@gmail.com>
2020-11-02 16:00:36 -0800

    did it bro

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+
+
+    before_action :find_employee, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @employees = Employee.all
+
+    end
+
+    def show   
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        redirect_to employee_path(@employee)
+    end
+
+    def edit
+         render new_employee_path
+    end
+
+    def update
+        if @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+        else 
+            render edit_employee_path
+        end
+    end
+
+    def destroy
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    private 
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
 end
